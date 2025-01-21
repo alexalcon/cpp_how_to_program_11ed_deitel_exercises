33c6459b7c20d42bdafcb3bc0e65c29c66106266
alexalcon <alexalcon1596@gmail.com>
2025-01-21 03:13:38 -0400

    Edit descriptions of exercise_3_26_v1_0_0.cpp and exercise_3_26_v1_1_0 from ch03

--- ch03/exercise_3_26_v1_0_0.cpp
+++ ch03/exercise_3_26_v1_0_0.cpp
@@ -3,21 +3,22 @@
  * Author: Alex Alcón
  * GitHub: https://github.com/alexalcon 
  * -------------------------------------------------------------------------------------------------
- * Description: (Enforcing Privacy with Cryptography) The explosive growth of Internet 
- *              communications and data storage on Internet-connected computers has greatly 
- *              increased privacy concerns. The field of cryptography is concerned with coding data
- *              to make it difficult (and hopefully—with the most advanced schemes—impossible) for 
- *              unauthorized users to read. In this exercise, you’ll investigate a simple scheme 
- *              for encrypting and decrypting data. Later in the book, you’ll learn sophisticated 
- *              secret-key and public-key cryptography. A company that wants to send data over the 
- *              Internet has asked you to write a program that will encrypt the data so that it may 
- *              be transmitted more securely. All the data is transmitted as four-digit integers.
- *              Your application should read a four-digit integer entered by the user and encrypt 
- *              it as follows: Replace each digit with the result of adding 7 to the digit and 
- *              getting the remainder after dividing the new value by 10. Then swap the first digit 
- *              with the third, and swap the second digit with the fourth. Then print the encrypted 
- *              integer. Write a separate application that inputs an encrypted four-digit integer
- *              and decrypts it (by reversing the encryption scheme) to form the original number.             
+ * Description: ✰ (Enforcing Privacy with Cryptography) The explosive growth of Internet 
+ *                 communications and data storage on Internet-connected computers has greatly 
+ *                 increased privacy concerns. The field of cryptography is concerned with coding 
+ *                 data to make it difficult (and hopefully—with the most advanced 
+ *                 schemes—impossible) for unauthorized users to read. In this exercise, you’ll 
+ *                 investigate a simple scheme for encrypting and decrypting data. Later in the 
+ *                 book, you’ll learn sophisticated secret-key and public-key cryptography. A 
+ *                 company that wants to send data over the Internet has asked you to write a 
+ *                 program that will encrypt the data so that it may be transmitted more securely. 
+ *                 All the data is transmitted as four-digit integers. Your application should read
+ *                 a four-digit integer entered by the user and encrypt it as follows: Replace each 
+ *                 digit with the result of adding 7 to the digit and getting the remainder after 
+ *                 dividing the new value by 10. Then swap the first digit with the third, and swap 
+ *                 the second digit with the fourth. Then print the encrypted integer. Write a 
+ *                 separate application that inputs an encrypted four-digit integer and decrypts it
+ *                 (by reversing the encryption scheme) to form the original number.               
  * -------------------------------------------------------------------------------------------------
  * Created on: 11-01-2025
  */
