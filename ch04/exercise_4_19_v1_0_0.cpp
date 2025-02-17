55e53cf58ab543dff3cd67d86d2411bccd2c4d8d
alexalcon <alexalcon1596@gmail.com>
2025-02-17 17:55:11 -0400

    Add exercises 16, 17, 18, 19 and 20 from ch04

---
+++ ch04/exercise_4_19_v1_0_0.cpp
@@ -0,0 +1,85 @@
+/**
+ * File: exercise_4_19_v1_0_0.cpp
+ * Author: Alex Alcón
+ * GitHub: https://github.com/alexalcon 
+ * -------------------------------------------------------------------------------------------------
+ * Description: ✰ (Diamond-Printing Program) Write an application that prints the following diamond
+ *                 shape. You may use output statements that print a single asterisk (*), a single
+ *                 space or a single newline character. Maximize your use of iteration (with nested 
+ *                 for statements), and minimize the number of output statements.
+ * 
+ *                               *
+ *                              ***
+ *                             *****
+ *                            *******
+ *                           *********
+ *                            *******
+ *                             *****
+ *                              ***
+ *                               *
+ * -------------------------------------------------------------------------------------------------
+ * Created on: 17-02-2025
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // initialization phase
+    //----------------------------------------------
+    int rows{9}; // number of rows
+    
+    int counter{0};
+    int first_column_index{rows/2};
+    int last_column_index{rows/2};
+    
+    if (rows % 2 == 0) {
+        rows += 1; // ensure an odd number of rows 
+                   // for a symmetric diamond
+    }
+    //----------------------------------------------
+    
+    // processing phase
+    //------------------------------------------------------------------------------------
+    for (int i{0} ; i < ( (rows * 2) - 1 ) ; ++i) {
+        cout << "-";
+    }
+    cout << "\n";
+    
+    // main logic 
+    // column printing logic
+    for (int row{0} ; row < rows ; ++row) {
+        for (int column{0} ; column < rows ; ++column) {
+            if ( ( column >= first_column_index ) && ( column <= last_column_index ) ) {
+                counter += 1;
+                cout << "* "; // "*" is used instead of a counter
+            }
+            else {
+                cout << "  "; 
+            } 
+        }
+        
+        // update first and last column indices
+        if ( row >= (rows / 2 ) ) {
+            // below middle row
+            first_column_index += 1;
+            last_column_index -= 1;
+        }
+        else {
+            // above middle row
+            first_column_index -= 1;
+            last_column_index += 1;
+        }
+        
+        cout << "\n";
+    }
+    
+    for (int i{0} ; i < ( (rows * 2) - 1 ) ; ++i) {
+        cout << "-";
+    }
+    cout << "\n";
+    //------------------------------------------------------------------------------------
+
+    return 0;
+}
