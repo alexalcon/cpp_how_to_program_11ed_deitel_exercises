964d191c40ee9c0df17530224f294121d8e7e0bf
alexalcon <alexalcon1596@gmail.com>
2025-11-25 18:08:06 -0400

    Update exercise 20 from ch04

--- ch04/exercise_4_20_v1_0_0.cpp
+++ ch04/exercise_4_20_v1_0_0.cpp
@@ -11,65 +11,67 @@
  * Created on: 17-02-2025
  */
 
- #include <iostream>
+#include <iostream>
 
- using namespace std;
+using namespace std;
  
- int main() {
-     // initialization phase
-     //----------------------------------------------
-     int rows{19}; // diamond order
-     
-     int counter{0};
-     int first_column_index{rows/2};
-     int last_column_index{rows/2};
-     
-     if (rows % 2 == 0) {
-         rows += 1; // ensure an odd number of rows 
-                   // for a symmetric diamond
-     }
-     //----------------------------------------------
-     
-     // processing phase
-     //------------------------------------------------------------------------------------
-     for (int i{0} ; i < ( (rows * 2) - 1 ) ; ++i) {
-         cout << "-";
-     }
-     cout << "\n";
-     
-     // main logic 
-     // column printing logic
-     for (int row{0} ; row < rows ; ++row) {
-         for (int column{0} ; column < rows ; ++column) {
-             if ( ( column >= first_column_index ) && ( column <= last_column_index ) ) {
-                 counter += 1;
-                 cout << "* "; // "*" is used instead of a counter
-             }
-             else {
-                 cout << "  "; 
-             } 
-         }
-         
-         // update first and last column indices
-         if ( row >= (rows / 2 ) ) {
-             // below middle row
-             first_column_index += 1;
-             last_column_index -= 1;
-         }
-         else {
-             // above middle row
-             first_column_index -= 1;
-             last_column_index += 1;
-         }
-         
-         cout << "\n";
-     }
-     
-     for (int i{0} ; i < ( (rows * 2) - 1 ) ; ++i) {
-         cout << "-";
-     }
-     cout << "\n";
-     //------------------------------------------------------------------------------------
- 
-     return 0;
- } 
+int main() {
+    // initialization phase
+    //----------------------------------------------
+    int rows{0}; // diamond order
+	cout << "Enter an odd number between 1 and 19 for the diamond order: ";
+    cin >> rows;
+
+	while ( (rows < 1) || (rows > 19) || (rows % 2 == 0) ) {
+		cout << "Invalid input. Please enter an odd number between 1 and 19: ";
+		cin >> rows;
+	}
+    
+    int counter{0};
+    int first_column_index{rows/2};
+    int last_column_index{rows/2};
+    //----------------------------------------------
+    
+    // processing phase
+    //------------------------------------------------------------------------------------
+    for (int i{0} ; i < ( (rows * 2) - 1 ) ; ++i) {
+        cout << "-";
+    }
+    cout << "\n";
+    
+    // main logic 
+    // column printing logic
+    for (int row{0} ; row < rows ; ++row) {
+        for (int column{0} ; column < rows ; ++column) {
+            if ( ( column >= first_column_index ) && ( column <= last_column_index ) ) {
+                counter += 1;
+                cout << "* "; // "*" is used instead of a counter
+            }
+            else {
+                cout << "  "; 
+            } 
+        }
+        
+        // update first and last column indices
+        if ( row >= (rows / 2 ) ) {
+            // below middle row
+            first_column_index += 1;
+            last_column_index -= 1;
+        }
+        else {
+            // above middle row
+            first_column_index -= 1;
+            last_column_index += 1;
+        }
+        
+        cout << "\n";
+    }
+    
+    for (int i{0} ; i < ( (rows * 2) - 1 ) ; ++i) {
+        cout << "-";
+    }
+    cout << "\n";
+    //------------------------------------------------------------------------------------
+
+    return 0;
+} 
